4e0cca23fb515e70f9471c8963180976acc38a57
OusmaneFane <ousmanefane08@gmail.com>
2023-04-12 08:43:32 +0000

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -348,7 +348,7 @@
             </h2>
           </div>
           <div class="col-lg-8">
-            <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem nesciunt quaerat ad reiciendis perferendis voluptate fugiat sunt fuga error totam.</p>
+            <p class="lead">la plateforme qui s'adapte à vos besoins d'apprentissage.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus assumenda omnis tempora ullam alias amet eveniet voluptas, incidunt quasi aut officiis porro ad, expedita saepe necessitatibus rem debitis architecto dolore? Nam omnis sapiente placeat blanditiis voluptas dignissimos, itaque fugit a laudantium adipisci dolorem enim ipsum cum molestias? Quod quae molestias modi fugiat quisquam. Eligendi recusandae officiis debitis quas beatae aliquam?</p>
             <p><a href="#">Read more</a></p>
           </div>
